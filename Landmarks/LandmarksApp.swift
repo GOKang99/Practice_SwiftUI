3b0c79dfa3277bbda34aea8912777ebf50ba881c
MacOmer <justletomer@gmail.com>
2025-01-30 01:15:46 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Omer on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
