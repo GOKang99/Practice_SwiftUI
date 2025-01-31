d28f1832437ae58355db2004319f1bacd21c276d
MacOmer <justletomer@gmail.com>
2025-01-31 19:33:33 +0900

    Featured추가

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -1,17 +1,34 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Omer on 1/30/25.
-//
-
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+
+
+    enum Tab {
+        case featured
+        case list
+    }
+
+
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+
+
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
+
 
 #Preview {
     ContentView()
